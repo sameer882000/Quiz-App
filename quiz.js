7e85b205921a472e74c3627024c6056b1fe8eea6
Sameer <sameer.882000@gmail.com>
2020-09-20 13:10:51 +0530

    final updates

--- quiz.js
+++ quiz.js
@@ -59,11 +59,11 @@ var quizOver = false;
 
 $(document).ready(function () {
 
-    // Display the first question
+    
     displayCurrentQuestion();
     $(this).find(".quizMessage").hide();
 
-    // On clicking next, display the next question
+    
     $(this).find(".nextButton").on("click", function () {
         if (!quizOver) {
 
@@ -73,26 +73,23 @@ $(document).ready(function () {
                 $(document).find(".quizMessage").text("Please select an answer");
                 $(document).find(".quizMessage").show();
             } else {
-                // TODO: Remove any message -> not sure if this is efficient to call this each time....
+             
                 $(document).find(".quizMessage").hide();
 
                 if (value == questions[currentQuestion].correctAnswer) {
                     correctAnswers++;
                 }
 
-                currentQuestion++; // Since we have already displayed the first question on DOM ready
+                currentQuestion++; 
                 if (currentQuestion < questions.length) {
                     displayCurrentQuestion();
                 } else {
                     displayScore();
-                    //                    $(document).find(".nextButton").toggle();
-                    //                    $(document).find(".playAgainButton").toggle();
-                    // Change the text in the next button to ask if user wants to play again
                     $(document).find(".nextButton").text("Play Again?");
                     quizOver = true;
                 }
             }
-        } else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+        } else { 
             quizOver = false;
             $(document).find(".nextButton").text("Next Question");
             resetQuiz();
@@ -103,7 +100,7 @@ $(document).ready(function () {
 
 });
 
-// This displays the current question AND the choices
+
 function displayCurrentQuestion() {
 
     console.log("In display current Question");
@@ -113,10 +110,10 @@ function displayCurrentQuestion() {
     var choiceList = $(document).find(".quizContainer > .choiceList");
     var numChoices = questions[currentQuestion].choices.length;
 
-    // Set the questionClass text to the current question
+   
     $(questionClass).text(question);
 
-    // Remove all current <li> elements (if any)
+    
     $(choiceList).find("li").remove();
 
     var choice;
